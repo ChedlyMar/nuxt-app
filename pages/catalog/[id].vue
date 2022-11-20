c8f9ff2305baf3cc1dcc2c86404f30fb7d8294ba
Chedly <chedly.marzougui@gmail.com>
2022-11-20 18:15:56 +0100

    Use route param page

---
+++ pages/catalog/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>catalog with id {{ id }} works !</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params;
+</script>
+
+<style scoped>
+</style>
