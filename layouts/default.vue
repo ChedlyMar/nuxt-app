47a32719d8432db981e5dd298babc0900fb7cd5d
Chedly <chedly.marzougui@gmail.com>
2022-11-20 18:07:02 +0100

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt App</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/home">Home</NuxtLink></li>
+          <li><NuxtLink to="/catalog">Catalog</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
